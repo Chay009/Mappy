c74a2d43fc30a82af62d7ced84d7452df40d833e
Toorubilli venkatachaitanya <chaitanyatoorubilli2409@gmail.com>
2023-02-19 03:28:55 +0530

    adding cache to make sure pwa is installable

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -11,3 +11,29 @@ self.addEventListener('activate',(event)=>{
 }) 
 
  // whenver service worker changes it will not updated automatically to do so close tab and open but creates bad user experience so skipwaiting is needed
+
+
+ // adding caches to make sure it becomes installable
+
+ self.addEventListener('install', (e) => {
+    e.waitUntil(
+      caches.open('Mappy-cache-1').then((cache) => cache.addAll([
+        '/',
+        '/index.html',
+        '/index.js',
+        '/style.css',
+        
+       
+      ])),
+    );
+  });
+  
+  
+  // this code shows add to home screen button in chrome
+  self.addEventListener('fetch', (e) => {
+    console.log(e.request.url);
+    e.respondWith(
+      caches.match(e.request).then((response) => response || fetch(e.request)),
+    );
+  });
+  
