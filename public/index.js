8a6ceec3a0ef8a067c0d410297ee72c88a365572
Toorubilli venkatachaitanya <chaitanyatoorubilli2409@gmail.com>
2023-03-06 19:41:58 +0530

    changed ui of map

--- public/index.js
+++ public/index.js
@@ -36,10 +36,23 @@ var osm = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 // CartoDB_DarkMatter.addTo(map_init);
 
 
+
+
+// Google Map Layer
+
+var googleStreets = L.tileLayer('http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}',{
+    
+    subdomains:['mt0','mt1','mt2','mt3'],
+    attribution: 'Chaitanya' 
+ });
+ googleStreets.addTo(map_init);
+
+
  // Satelite Layer
 var googleSat = L.tileLayer('http://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}',{
   
-   subdomains:['mt0','mt1','mt2','mt3']
+   subdomains:['mt0','mt1','mt2','mt3'],
+   attribution: 'Chaitanya' 
  });
 googleSat.addTo(map_init);
 
@@ -52,22 +65,18 @@ googleSat.addTo(map_init);
 // Stamen_Watercolor.addTo(map_init);
 
 
-// Google Map Layer
 
-var googleStreets = L.tileLayer('http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}',{
-    
-    subdomains:['mt0','mt1','mt2','mt3']
- });
- googleStreets.addTo(map_init);
 
 
 
 var baseLayers = {
  
-  "Google Map":googleStreets,
+  // these layers should be opposite ie bottom layer is shown first
   // "Water Color":Stamen_Watercolor,
+   "OpenStreetMap": osm,
   "Satellite":googleSat,
-  "OpenStreetMap": osm,
+  "Google Map":googleStreets,
+ 
 };
 
 L.control.layers(baseLayers).addTo(map_init);
@@ -89,13 +98,26 @@ if(!navigator.geolocation) {
 
 
 var marker, circle, lat, long, accuracy;
-var svg = '<svg id="mePin" xmlns="http://www.w3.org/2000/svg" width="43.3" height="42.4" viewBox="0 0 43.3 42.4"><path class="ring_outer" fill="#878787" d="M28.6 23c6.1 1.4 10.4 4.4 10.4 8 0 4.7-7.7 8.6-17.3 8.6-9.6 0-17.4-3.9-17.4-8.6 0-3.5 4.2-6.5 10.3-7.9.7-.1-.4-1.5-1.3-1.3C5.5 23.4 0 27.2 0 31.7c0 6 9.7 10.7 21.7 10.7s21.6-4.8 21.6-10.7c0-4.6-5.7-8.4-13.7-10-.8-.2-1.8 1.2-1 1.4z"/><path class="ring_inner" fill="#5F5F5F" d="M27 25.8c2 .7 3.3 1.8 3.3 3 0 2.2-3.7 3.9-8.3 3.9-4.6 0-8.3-1.7-8.3-3.8 0-1 .8-1.9 2.2-2.6.6-.3-.3-2-1-1.6-2.8 1-4.6 2.7-4.6 4.6 0 3.2 5.1 5.7 11.4 5.7 6.2 0 11.3-2.5 11.3-5.7 0-2-2.1-3.9-5.4-5-.7-.1-1.2 1.3-.7 1.5z"/><path class="mePin" d="M21.6 8.1a4 4 0 0 0 4-4 4 4 0 0 0-4-4.1 4.1 4.1 0 0 0-4.1 4 4 4 0 0 0 4 4.1zm4.9 8v-3.7c0-1.2-.6-2.2-1.7-2.6-1-.4-1.9-.6-2.8-.6h-.9c-1 0-2 .2-2.8.6-1.2.4-1.8 1.4-1.8 2.6V16c0 .9 0 2 .2 2.8.2.8.8 1.5 1 2.3l.2.3.4 1 .1.8.2.7.6 3.6c-.6.3-.9.7-.9 1.2 0 .9 1.4 1.7 3.2 1.7 1.8 0 3.2-.8 3.2-1.7 0-.5-.3-.9-.8-1.2l.6-3.6.1-.7.2-.8.3-1 .1-.3c.3-.8 1-1.5 1.1-2.3.2-.8.2-2 .2-2.8z" fill="#282828"/></svg>';
+// var svg = '<svg id="mePin" xmlns="http://www.w3.org/2000/svg" width="43.3" height="42.4" viewBox="0 0 43.3 42.4"><path class="ring_outer" fill="#878787" d="M28.6 23c6.1 1.4 10.4 4.4 10.4 8 0 4.7-7.7 8.6-17.3 8.6-9.6 0-17.4-3.9-17.4-8.6 0-3.5 4.2-6.5 10.3-7.9.7-.1-.4-1.5-1.3-1.3C5.5 23.4 0 27.2 0 31.7c0 6 9.7 10.7 21.7 10.7s21.6-4.8 21.6-10.7c0-4.6-5.7-8.4-13.7-10-.8-.2-1.8 1.2-1 1.4z"/><path class="ring_inner" fill="#5F5F5F" d="M27 25.8c2 .7 3.3 1.8 3.3 3 0 2.2-3.7 3.9-8.3 3.9-4.6 0-8.3-1.7-8.3-3.8 0-1 .8-1.9 2.2-2.6.6-.3-.3-2-1-1.6-2.8 1-4.6 2.7-4.6 4.6 0 3.2 5.1 5.7 11.4 5.7 6.2 0 11.3-2.5 11.3-5.7 0-2-2.1-3.9-5.4-5-.7-.1-1.2 1.3-.7 1.5z"/><path class="mePin" d="M21.6 8.1a4 4 0 0 0 4-4 4 4 0 0 0-4-4.1 4.1 4.1 0 0 0-4.1 4 4 4 0 0 0 4 4.1zm4.9 8v-3.7c0-1.2-.6-2.2-1.7-2.6-1-.4-1.9-.6-2.8-.6h-.9c-1 0-2 .2-2.8.6-1.2.4-1.8 1.4-1.8 2.6V16c0 .9 0 2 .2 2.8.2.8.8 1.5 1 2.3l.2.3.4 1 .1.8.2.7.6 3.6c-.6.3-.9.7-.9 1.2 0 .9 1.4 1.7 3.2 1.7 1.8 0 3.2-.8 3.2-1.7 0-.5-.3-.9-.8-1.2l.6-3.6.1-.7.2-.8.3-1 .1-.3c.3-.8 1-1.5 1.1-2.3.2-.8.2-2 .2-2.8z" fill="#282828"/></svg>';
+var svg = `<svg width="60" height="60" viewBox="0 0 60 60">
+<circle cx="30" cy="30" r="7" fill="#FFA631" />
+<circle cx="30" cy="30" r="7" fill="#22A7F0" opacity="0.8">
+  <animate attributeName="r" from="7.5" to="20" dur="1.5s" repeatCount="indefinite" />
+  <animate attributeName="opacity" from="0.8" to="0" dur="1.5s" repeatCount="indefinite" />
+</circle>
+</svg>
+
+
+
+`
+
 
 	var meIcon = L.divIcon({
 		className: "leaflet-data-marker",
-			html: svg.replace('#','%23'),
+			html: svg.replace('#','%25'),
 
-			iconAnchor  : [22, 28],
+      
+			iconAnchor  : [9, 12],
 			iconSize    : [36, 42],
 			popupAnchor : [0, -30],
 		});
